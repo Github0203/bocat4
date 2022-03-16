0c33bf4dcfd978c62f7292e7b64bb6990e08e7ca
Github0203 <riddnicky0203@gmail.com>
2022-03-16 16:48:59 +0700

    login-logout

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,9 @@
 import {BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login/Login";
 import Bocatvang from "./pages/gioithieu/Bocatvang";
 import Gioithieuvecongty from "./pages/gioithieu/Gioithieuvecongty";
 import Home from "./pages/Home";
+import Register from "./components/Login/Register";
 
 
 const Router = () => {
@@ -9,6 +11,8 @@ const Router = () => {
         <BrowserRouter>
         <Routes>
             <Route path='/bocatvang' element={<Bocatvang /> } />
+            <Route path='/login' element={<Login /> } />
+            <Route path='/register' element={<Register /> } />
             <Route path='/about' element={<Gioithieuvecongty />} />
             <Route exact path="/" element={<Home />} />
             </Routes>
--- src/components/header/topheader/Ngonngu.js
+++ src/components/header/topheader/Ngonngu.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Ngonngu extends Component {
   render() {
     return (
       <div className='ngonngu'>
-          <a href='=#'><img src='https://yes8asia.com/wp-content/uploads/2020/11/yes8asia_vn.png' /></a>
+        <Link to="/login">Login</Link>
+          <a href='=#'><img src='https://www.pngkit.com/png/detail/382-3826500_vietnam-flag-circle-icon-vietnam-flag-flags-of.png' /></a>
           <a href='=#'><img src='https://bocatvangtravel.com.vn/images/topicon_lanen.png' /></a>
       </div>
     )
